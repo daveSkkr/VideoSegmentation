625e6384c18b659c7b4d794eba18958bcbd5f684
sikor <sikorski.daw9@gmail.com>
2023-05-16 19:55:15 +0200

    Add Unet model

--- unet/DoubleConv2d.py
+++ unet/DoubleConv2d.py
@@ -1,5 +1,6 @@
-
+import torch
 import torch.nn as nn
+import torchvision.transforms.functional as TF
 
 class DoubleConv2d(nn.Module):
     
@@ -11,19 +12,21 @@ class DoubleConv2d(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias = False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
-        self.conv(x)
+        return self.conv(x)
 
-class UNet(nn.Module):
+class UNETScapes(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, features):
+        super(UNETScapes, self).__init__()
 
-    def __int__(self, in_channels = 3, out_channels = 3, features = [64, 128, 256, 512]):
-        super(UNet, self).__int__()
-
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.features = features
         self.downs = nn.ModuleList()
         self.ups = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -35,14 +38,13 @@ class UNet(nn.Module):
             self.downs.append(DoubleConv2d(in_channels, feature))
             in_channels = feature
 
+        self.bottleNeck = DoubleConv2d(features[-1], features[-1] * 2)
+
         # ups
         for feature in reversed(features):
-            self.ups.append(
-                nn.ConvTranspose2d(feature * 2, feature, kernel_size= 2, stride=2)
-            )
+            self.ups.append(nn.ConvTranspose2d(feature * 2, feature, kernel_size= 2, stride=2))
             self.ups.append(DoubleConv2d(feature * 2, feature))
 
-        self.bottleNeck = DoubleConv2d(features[-1], features[-1] * 2)
         self.finalConv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
     def forward(self, x):
@@ -55,13 +57,28 @@ class UNet(nn.Module):
 
         x = self.bottleNeck(x)
 
-        skipConnections = reversed(skipConnections)
+        skipConnections = skipConnections[::-1]
+
         for idx in range(0, len(self.ups), 2):
             x = self.ups[idx](x)
             skipConn = skipConnections[idx//2]
-            skipConn = torch.cat(skipConn, x, dim=1)
-            x = self.ups[idx +1 ](x)
+
+            if x.shape != skipConn.shape:
+                x = TF.resize(x, size = skipConn.shape[2:])
+
+            concatSkip = torch.cat((skipConn, x), dim=1)
+            x = self.ups[idx +1](concatSkip)
 
         return self.finalConv(x)
+
+def test():
+    x = torch.randn((3, 1, 161, 161)) # batch_size, channels, size
+    model = UNETScapes(in_channels = 1, out_channels = 1, features = [64, 128, 256, 512])
+    preds = model(x)
+
+    assert preds.shape == x.shape
+
+if __name__ == '__main__':
+    test()
 
 
