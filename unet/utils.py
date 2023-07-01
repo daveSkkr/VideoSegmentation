d88b5f231f77a7e03d8590ef0e28ffe7265a4f4a
sikor <sikorski.daw9@gmail.com>
2023-07-01 21:51:28 +0200

    Add sample to evaluate

--- unet/utils.py
+++ unet/utils.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 import numpy as np
 from PIL import Image
 from torch import Tensor
+import matplotlib.pyplot as plt
 
 def dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
     # Average of Dice coefficient for all batches, or for a single mask
@@ -30,3 +31,26 @@ def dice_loss(input: Tensor, target: Tensor, multiclass: bool = False):
     # Dice loss (objective to minimize) between 0 and 1
     fn = multiclass_dice_coeff if multiclass else dice_coeff
     return 1 - fn(input, target, reduce_batch_first=True)
+
+def show_predictions_sample_plot(model, val_loader, inverse_transforms, batch_size_val, device):
+
+	X, Y = next(iter(val_loader))
+	X, Y = X.to(device), Y.to(device)
+	Y_pred = model(X)
+	print(Y_pred.shape)
+	Y_pred = torch.argmax(Y_pred, dim=1)
+	print(Y_pred.shape)
+	fig, axes = plt.subplots(batch_size_val, 3, figsize=(3 * 5, 5 * 5))
+
+	for i in range(batch_size_val):
+		landscape = inverse_transforms(X[i]).permute(1, 2, 0).cpu().detach().numpy()
+		label_class = Y[i].cpu().detach().numpy()
+		label_class_predicted = Y_pred[i].cpu().detach().numpy()
+
+		axes[i, 0].imshow(landscape)
+		axes[i, 0].set_title("Landscape")
+		axes[i, 1].imshow(label_class)
+		axes[i, 1].set_title("Label Class")
+		axes[i, 2].imshow(label_class_predicted)
+		axes[i, 2].set_title("Label Class - Predicted")
+	plt.show()
